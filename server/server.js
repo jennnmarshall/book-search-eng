a919592c53c7a76a541e6930fb38cd4b3740b7b8
Jenn Marshall <jennyfever@gmail.com>
2023-01-16 14:30:28 -0700

    changes to server and resolvers

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
-
-
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
+const { authMiddleware } = require("./utils/auth");
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
@@ -12,6 +11,7 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 app.use(express.urlencoded({ extended: false }));
@@ -42,4 +42,3 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
-
